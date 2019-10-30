dda6ff8e1a7ff460b0c74da78b510a6bb1f32396
Samuele Sabella <sabella@ntop.org>
2019-10-30 12:41:31 +0100

    flask web server

--- image_analysis/main.py
+++ image_analysis/main.py
@@ -1,27 +1,43 @@
 import io
 import os
 
-# Imports the Google Cloud client library
+from PIL import Image
+from flask import Flask, escape, request
 from google.cloud import vision
 from google.cloud.vision import types
 
+
+# ----- ENVIRONMENT ----- #
+app = Flask(__name__)
+set_key()
+client = vision.ImageAnnotatorClient()
+
+
+# ----- ROUTES ----- #
+@app.route('/upload', methods=['POST'])
+def upload():
+    api_res = get_vision(request.files['file'].read())
+    print(api_res)
+    return 'Success!'
+
+
+# ----- FUNCTION DEFINITION ----- #
 def set_key(key_path='key/vision_api_keys.json'):
   os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=key_path
 
-def get_vision (image_path):
+def get_vision (content):
   # Instantiates a client
   client = vision.ImageAnnotatorClient()
-  # TODO: merge json reponse
-  
+
   # The name of the image file to annotate
-  file_name = os.path.abspath(image_path)
-  
-  # Loads the image into memory
-  with io.open(file_name, 'rb') as image_file:
-      content = image_file.read()
-  
   image = types.Image(content=content)
   
   # Performs label detection on the image file
-  return client.label_detection(image=image)
+  label = client.label_detection(image=image)
+  web_entities = client.web_detection(image=image)
+  merge_res = {
+   "label": label,
+   "web_entities": web_entities
+  }
+  return merge_res
 
