6911fb194d85049471a3c490ce61ba4aee2c4908
Emanuele Aurora <Emanuele.aurora@gmail.com>
2019-10-30 17:34:41 +0100

    PEP8 format on main and README
    also added .pyc files on gitignore

--- image_analysis/README.md
+++ image_analysis/README.md
@@ -1,15 +1,36 @@
-# Start server (linux/mac)
-Start server on port 5000
-```
-$ env FLASK_APP=main.py flask run
-```
-# Start server (linux/mac)
-Start server on port 5000
-```
-$ set FLASK_APP=main.py flask run
-```
-# Client
-```
-curl -X POST -F "file=@resources/images_test.jpg" localhost:5000/upload
+
+# Add FLASK_APP env (linux/mac)
+
+```console
+$ env FLASK_APP=main.py
+$
 ```
 
+# Add FLASK_APP env (windows)
+
+```console
+$ set FLASK_APP=main.py
+$
+```
+
+# Start server
+
+Start server on port 5000
+
+```console
+$ flask run
+* Serving Flask app "main.py" (lazy loading)
+* Environment: development
+* Debug mode: on
+* Restarting with stat
+* Debugger is active!
+* Debugger PIN: 302-116-947
+* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+```
+
+# Client
+
+```console
+$ curl -X POST -F "file=@resources/images_test.jpg" localhost:5000/upload
+Success!
+```
--- image_analysis/main.py
+++ image_analysis/main.py
@@ -1,30 +1,31 @@
 import io
 import os
 
-from PIL import Image
 from flask import Flask, escape, request
 from google.cloud import vision
 from google.cloud.vision import types
 
+
 # ----- FUNCTION DEFINITION ----- #
 def set_key(key_path='key/vision_api_keys.json'):
-  os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=key_path
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
-def get_vision (content):
-  # Instantiates a client
-  client = vision.ImageAnnotatorClient()
 
-  # The name of the image file to annotate
-  image = types.Image(content=content)
-  
-  # Performs label detection on the image file
-  label = client.label_detection(image=image)
-  web_entities = client.web_detection(image=image)
-  merge_res = {
-    "label": label,
-    "web_entities": web_entities
-  }
-  return merge_res
+def get_vision(content):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    label = client.label_detection(image=image)
+    web_entities = client.web_detection(image=image)
+    merge_res = {
+        "label": label,
+        "web_entities": web_entities
+    }
+    return merge_res
 
 
 # ----- ENVIRONMENT ----- #
@@ -36,8 +37,6 @@ client = vision.ImageAnnotatorClient()
 # ----- ROUTES ----- #
 @app.route('/upload', methods=['POST'])
 def upload():
-  api_res = get_vision(request.files['file'].read())
-  print(api_res)
-  return 'Success!'
-
-
+    api_res = get_vision(request.files['file'].read())
+    print(api_res)
+    return 'Success!'
