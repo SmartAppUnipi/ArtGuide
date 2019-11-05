761d198396eb70a82bd7fcdb4105f6a894f02062
Matteo <matteo@minint-npqpj7f.homenet.telecomitalia.it>
2019-11-05 10:56:21 +0100

    aggiunta cartella demo contenente test, in particolare una procedura che estrae le parole salienti dei topic che contraddistinguono gli articoli usando LDA e un meccanismo di clustering che utilizza word embeddings GloVe e Word2Vec per selezionare l'articolo più affine al topic desiderato dall'utente

--- adaptation/api.py
+++ adaptation/api.py
@@ -1,6 +1,5 @@
 # PEP8 - 4 spazi
 
-
 from flask import Flask
 import json
 from document_adaptation import DocumentAdaptation
@@ -8,14 +7,14 @@ from document_adaptation import DocumentAdaptation
 app = Flask(__name__)
 
 @app.route('/keywords', method="POST")
-def home():
+def keywords():
     req = request.get_json()
     results = DocumentAdaptation.get_keywords(req['user_info'])
     req['keywords'] = results 
     return jsonify(req)
 
 @app.route('/tailored_text', method="POST")
-def home():
+def tailored_text():
     req = request.get_json()
     results = DocumentAdaptation.get_tailored_text(req['texts'])
     req['tailored_text'] = results
