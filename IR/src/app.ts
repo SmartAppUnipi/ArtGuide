ee6efdb47cde610f76bd0ccd30c2d60c2caf1dd5
Chiara <baragliachiara@yahoo.it>
2019-11-05 14:09:03 +0100

    merged

--- IR/src/app.ts
+++ IR/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { GoogleSearchRouter } from './google-search';
 import { ScrapingRouter } from './scraping';
+import wiki from 'wikijs';
 
 // Create a new express application instance
 const app: express.Application = express();
@@ -14,6 +15,13 @@ app.get('/', (req, res) => {
 // Use the router defined into that module
 app.use('/google-search', GoogleSearchRouter)
 app.use('/scrape', ScrapingRouter)
+app.get('/batman', (req, res) => {
+  wiki().page('batman').then(page => page.content()).then(content=> res.json(content));
+});
+
+app.get('/wiki', (req, res) => {
+  wiki().search('Gioconda').then(data => wiki().page(data.results[0]).then(page => page.categories()).then(console.log));
+});
 
 export default app;
 
