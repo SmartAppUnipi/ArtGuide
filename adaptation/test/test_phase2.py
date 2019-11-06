fdc83d7eba7949c41bfed1f745a01ff1be53783a
PietroPigLonardi <pietrolonardi@gmail.com>
2019-11-06 17:07:29 +0100

    Update test file
    Added the possibility to insert a json file to test (default is input_phase1.json for phase1 and input_phase2.json for phase2)
    Output formatted and indented

--- adaptation/test/test_phase2.py
+++ adaptation/test/test_phase2.py
@@ -4,10 +4,17 @@ import os
   
 URL = "http://cipizio.it:4321/tailored_text"
    
-with open("./adaptation/data/input_phase2.json") as file:
+path="./adaptation/data/"
+
+file=input("Insert file for phase 2(must be in data folder): ")
+
+if file == "":
+    file = "input_phase2.json"
+ 
+with open(path+file) as file:
     PARAMS = json.load(file)
     r = requests.post(url = URL, json = PARAMS) 
   
 data = r.json() 
-  
-print(data)
+
+print(json.dumps(data, indent=2, sort_keys=True))
