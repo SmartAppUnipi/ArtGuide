fa1c2a0b8a0bc1a10d1f8e81992471e110917ab7
Matteo <matteo@minint-npqpj7f.homenet.telecomitalia.it>
2019-11-11 01:37:47 +0100

    imporved document selection, now based on BERT Sentences embedding of salient sentences extraced using RAKE and TextRank, also take in to consideration the readability of conent

--- adaptation/document_adaptation/__init__.py
+++ adaptation/document_adaptation/__init__.py
@@ -1,2 +1,3 @@
 from .documents_adaptation import DocumentsAdaptation
+from .semantic_search import BERT_distance, Semantic_Search
 from .user import User
--- adaptation/document_adaptation/user.py
+++ adaptation/document_adaptation/user.py
@@ -1,6 +1,7 @@
 
 class User():
+    ''' User interface '''
     def __init__(self, user_profile):
         self.tastes = user_profile['tastes']
-        self.expertise_level = user_profile['expertiseLevel'] # enum from ["child","novice","knowledgeable","expert"]
+        self.expertise_level = int(user_profile['expertiseLevel']) # enum from ["child","novice","knowledgeable","expert"]
         self.language = user_profile['language']
