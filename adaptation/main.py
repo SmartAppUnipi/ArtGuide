3aefdbc13b18e2f52cb0dcdec88c8aeb5a5624fa
PietroPigLonardi <pietrolonardi@gmail.com>
2019-11-17 16:42:47 +0100

    Update main.py
    Correct the name of the attribute snakecase->camelcase
    added basic handler for internal server error

--- adaptation/main.py
+++ adaptation/main.py
@@ -30,11 +30,12 @@ def keywords():
     if not req:
         return abort(400) # BAD REQUEST
     if 'userProfile' not in req:
-        req['adaption_error'] = {"userProfile not found"}
+        req['adaptionError'] = {"userProfile not found"}
         return jsonify(req)
     if 'tastes' not in req['userProfile']:
-        req['adaption_error'] = {"userProfile incomplete"}
-    
+        req['adaptionError'] = {"userProfile incomplete"}
+		
+	
     # Body
     user = User(req["userProfile"])
     results = document_adaptation.get_keywords(user.tastes)
@@ -50,13 +51,13 @@ def tailored_text():
     if not req:
         return abort(400) # BAD REQUEST
     if  'results' not in req:
-        req['adaption_error'] = {"results not found"}
+        req['adaptionError'] = {"results not found"}
         return jsonify(req)
     if 'userProfile' not in req:
-        req['adaption_error'] = {"userProfile not found"}
+        req['adaptionError'] = {"userProfile not found"}
         return jsonify(req)
     if 'tastes' not in req['userProfile'] or 'expertiseLevel' not in req['userProfile'] or 'language' not in req['userProfile']:
-        req['adaption_error'] = {"userProfile incomplete"}
+        req['adaptionError'] = {"userProfile incomplete"}
        
     # Body
     user = User(req["userProfile"])
@@ -64,6 +65,12 @@ def tailored_text():
     req['tailoredText'] = results
     return jsonify(req)
 
+@app.errorhandler(500)
+def internal_error(exc):
+    req = request.get_json()
+    #print("\n",exc,"\n")
+    req['adaption_error'] = "500 - Internal Server Error"
+    return jsonify(req)
 
 if __name__ == '__main__':
     app.run(debug=config.debug, host= '0.0.0.0', port=config.port)
