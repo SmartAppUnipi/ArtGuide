95f50f9f9a1431abe822b4014f86e4064bd80ffe
Samuele Sabella <sabella@ntop.org>
2019-11-19 17:47:44 +0100

    Fix before refactoring

--- image_analysis/models/project_types.py
+++ image_analysis/models/project_types.py
@@ -7,6 +7,10 @@ checkpoints = Path('./res').resolve()
 datasets = Path('./data/').resolve()
 
 # ----- files ----- #
+art_or_building = datasets / 'art_or_building_v1'
+pict_style = datasets / 'pict_style_v1'
+arch_style = datasets / 'arch_style_v1'
+
 arch_dset = datasets / 'arcDataset'
 pict_dset = datasets / 'train_1'
 pict_info = datasets / 'all_data_info.csv'
