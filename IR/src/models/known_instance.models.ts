73d150dc2c0c2ba18ed98604bdc2018bd868ddeb
Chiara <baragliachiara@yahoo.it>
2019-11-26 17:54:15 +0100

    Check if an instance is known (wikidata module)

---
+++ IR/src/models/known_instance.models.ts
@@ -0,0 +1,9 @@
+export interface KnownInstance{
+    Creator: Array<string>;
+    Genre: Array<string>;
+    Movement: Array<string>;
+    Architect: Array<string>;
+    Architectural_style: Array<string>;
+    Location: Array<string>;
+    WikipediaPageTitle: string;
+}
