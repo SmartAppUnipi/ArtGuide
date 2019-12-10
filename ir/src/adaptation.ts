99de77f025e11470ddb39279d74bcf1ec6faa279
daquinoaldo <daquino.aldo@gmail.com>
2019-12-10 16:11:41 +0100

    Improve fuzzy tests

--- ir/src/adaptation.ts
+++ ir/src/adaptation.ts
@@ -24,11 +24,16 @@ export class Adaptation {
      * @returns A promise resolved with the received JSON parsed as JS object of type T.
      */
     private post<T = any>(url: string, body: any): Promise<T> {
-        return fetch(url, {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: { "Content-Type": "application/json" }
-        }).then(res => res.json());
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: "POST",
+                body: JSON.stringify(body),
+                headers: { "Content-Type": "application/json" }
+            }).then(res => {
+                if (res.status !== 200) reject(res)
+                resolve(res.json())
+            });
+        })
     }
 
     /**
