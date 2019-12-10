076d7bc197cf4ff999a05d02a8a547a52967e911
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2019-12-10 15:11:05 +0100

    Delete log file before a new execution

--- ir/src/index.ts
+++ ir/src/index.ts
@@ -1,5 +1,13 @@
 import app from "./app";
-import { ExpressPort } from "./environment";
+import { ExpressPort, LoggerConfig } from "./environment";
+import fs from "fs";
+
+if (LoggerConfig.file) {
+    // delete log file before starting a new execution
+    if (fs.existsSync(LoggerConfig.file)) {
+        fs.unlinkSync(LoggerConfig.file);
+    }
+}
 
 app.listen(ExpressPort, () => {
     // eslint-disable-next-line
