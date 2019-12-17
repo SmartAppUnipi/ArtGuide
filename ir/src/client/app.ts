3df097d00be0acae24bb38227dbeffa2d214364b
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2019-12-17 12:15:41 +0100

    Add spans to highlight tailored text matches

--- ir/src/client/app.ts
+++ ir/src/client/app.ts
@@ -1,6 +1,45 @@
 import { LogFormatter } from "./log-formatter";
 
+function generateId(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function getRandomColor() {
+    const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#00B3E6',
+        '#E6B333', '#3366E6', '#999966', '#47ef47', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+        '#FF99E6', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+        '#4D8066', '#809980', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+    return colorArray[Math.floor(Math.random() * colorArray.length)]
+}
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
 function find(tailoredText: string, pageResultJson: string) {
+    const result: {
+        spans: Array<{ irSpanId: string, text: string }>,
+        html: string
+    } = { spans: [], html: "" };
+
     const matches = []
     let i = 0;
     while (i < tailoredText.length) {
@@ -34,14 +73,19 @@ function find(tailoredText: string, pageResultJson: string) {
     matches.sort((a, b) => b.start - a.start)
     for (let match of matches) {
         const before = pageResultJson.substring(0, match.start);
-        const span = `<span style="color: red">${match.text}</span>`;
+        const spanId = generateId(4);
+        const textColor = getRandomColor();
+        // for each matched span, keep a reference to its id
+        result.spans.push({ irSpanId: spanId, text: match.text });
+        const span = `<span style="color: ${textColor}" id="${spanId}" data-color="${textColor}">${match.text}</span>`;
         const after = pageResultJson.substring(match.end);
         pageResultJson = before + span + after;
     }
 
-    return pageResultJson;
-}
+    result.html = pageResultJson
 
+    return result;
+}
 
 (async () => {
     const irTree = document.getElementById("ir-tree");
@@ -54,13 +98,45 @@ function find(tailoredText: string, pageResultJson: string) {
     const adaptationResponseLogs = logs.filter(l => l.message == "[adaptation.ts] Adaptation text response");
     const last = adaptationResponseLogs?.pop();
 
-    const tailoredText = last?.metadata?.adaptationResponse?.tailoredText ?? "Log not found";
+    const tailoredText: string = last?.metadata?.adaptationResponse?.tailoredText ?? "Log not found";
     const pageResults = last?.metadata?.adaptationResponse?.results ?? "Log not found";
-    const pageResultJson = JSON.stringify(pageResults, null, 2);
+    const pageResultJson: string = JSON.stringify(pageResults, null, 2);
 
-    irTree.innerText = pageResultJson;
-    adaptationTree.innerText = tailoredText;
 
-    irTree.innerHTML = find(tailoredText, pageResultJson);
+    // create the html with the matched text wrapped in a colored span
+    const logParsingResult = find(tailoredText, escapeHtml(pageResultJson));
+
+    // assign to the left div
+    irTree.innerHTML = logParsingResult.html;
+
+
+    // for each matched span, create the corresponding span on the right div
+    let adaptationHtml = "";
+    for (const span of logParsingResult.spans) {
+        // take the color from the attribute set before
+        const color = document.getElementById(span.irSpanId).getAttribute("data-color");
+        // create the new html string
+        adaptationHtml += `<span style="color: ${color}" data-ir-span-id="${span.irSpanId}" class="adaptation-span">${span.text}</span>`;
+    }
+
+    // assign it to the right div
+    adaptationTree.innerHTML = adaptationHtml;
+
+    for (const adaptationSpan of document.getElementsByClassName("adaptation-span")) {
+        // for each right span (from adaptation tailored text) add a click listsner
+        adaptationSpan.addEventListener("click", function (e) {
+
+            // take the corresponding right div to obtain its position
+            const currentSpanId = this.getAttribute("data-ir-span-id");
+            const span = document.getElementById(currentSpanId);
+            
+            // get top and left position 
+            const topPos = span.offsetTop - 50;
+            const leftPos = span.offsetLeft - 10;
+
+            // scroll the parent div until the right div is visible
+            document.getElementById("ir-tree").scrollTop = topPos;
+            document.getElementById("ir-tree").scrollLeft = leftPos;
+        });
+    }
 })();
-
